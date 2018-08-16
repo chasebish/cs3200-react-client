fcd4a17227a3b70b77850293a447e28db795ab01
chasebish <bishop.c@husky.neu.edu>
2018-08-16 10:32:39 -0400

    home style

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -70,6 +70,10 @@ class HomeComponent extends React.Component {
     cuteSort = () => this.renderVideos(this.props.videos.sort((a, b) => b.avgCuteness - a.avgCuteness))
     sadSort = () => this.renderVideos(this.props.videos.sort((a, b) => b.avgSadness - a.avgSadness))
 
+    active = sortType => {
+        if (this.state.sort === sortType) { return 'active' }
+    }
+
 
     render() {
         return (
@@ -77,15 +81,15 @@ class HomeComponent extends React.Component {
                 <h1 className='display-3'>Rotten Potatoes</h1>
                 <div className='text-center'>
                     <div className="btn-group-sm" role="group" aria-label="Basic example">
-                        <button onClick={() => this.viewState()} type="button" className="btn btn-primary m-1">Views</button>
-                        <button onClick={() => this.likeState()} type="button" className="btn btn-primary m-1">Likes</button>
-                        <button onClick={() => this.dislikeState()} type="button" className="btn btn-primary m-1">Dislikes</button>
-                        <button onClick={() => this.overallState()} type="button" className="btn btn-primary m-1">Overall</button>
-                        <button onClick={() => this.humorState()} type="button" className="btn btn-primary m-1">Humor</button>
-                        <button onClick={() => this.informativeState()} type="button" className="btn btn-primary m-1">Informativeness</button>
-                        <button onClick={() => this.productionState()} type="button" className="btn btn-primary m-1">Production</button>
-                        <button onClick={() => this.cuteState()} type="button" className="btn btn-primary m-1">Cuteness</button>
-                        <button onClick={() => this.sadState()} type="button" className="btn btn-primary m-1">Sadness</button>
+                        <button onClick={() => this.viewState()} type="button" className={`btn btn-outline-info m-1 ${this.active('VIEW')}`}>Views</button>
+                        <button onClick={() => this.likeState()} type="button" className={`btn btn-outline-info m-1 ${this.active('LIKE')}`}>Likes</button>
+                        <button onClick={() => this.dislikeState()} type="button" className={`btn btn-outline-info m-1 ${this.active('DISLIKE')}`}>Dislikes</button>
+                        <button onClick={() => this.overallState()} type="button" className={`btn btn-outline-info m-1 ${this.active('OVERALL')}`}>Overall</button>
+                        <button onClick={() => this.humorState()} type="button" className={`btn btn-outline-info m-1 ${this.active('HUMOR')}`}>Humor</button>
+                        <button onClick={() => this.informativeState()} type="button" className={`btn btn-outline-info m-1 ${this.active('INFORMATIVE')}`}>Informativeness</button>
+                        <button onClick={() => this.productionState()} type="button" className={`btn btn-outline-info m-1 ${this.active('PRODUCTION')}`}>Production</button>
+                        <button onClick={() => this.cuteState()} type="button" className={`btn btn-outline-info m-1 ${this.active('CUTE')}`}>Cuteness</button>
+                        <button onClick={() => this.sadState()} type="button" className={`btn btn-outline-info m-1 ${this.active('SAD')}`}>Sadness</button>
                     </div>
                 </div>
                 {this.state.sort === 'VIEW' && this.viewSort()}
